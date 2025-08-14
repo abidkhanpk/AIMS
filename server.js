340c7258c3b376205a01362cbec77f444eb6b4ef
Abid Khan <abid@gmx.com>
2025-08-14 11:28:34 +0500

    Server successfully started.

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(session({
 }));
 
 // CSRF protection middleware
-const csrfProtection = csrf({ cookie: true });
+const csrfProtection = csrf();
 app.use(csrfProtection);
 
 // Make CSRF token available to all views
